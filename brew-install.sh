60deedea3516a4ea40ab63646d9f8ef65d3bc7e3
Jeremy Vinding <jeremy.vinding@closely.com>
2013-10-04 19:42:36 -0600

    force running brew-install as admin and add helpful info it it or brew-packages is not run as admin

--- brew-install.sh
+++ brew-install.sh
@@ -1,1 +1,8 @@
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+#!/bin/bash
+. ~/Environment/mac_shortcuts
+if is_admin; then
+    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+else
+    echo Must be run as admin. Try:
+	echo "su admin -c \"$0\""
+fi
--- brew-packages.sh
+++ brew-packages.sh
@@ -3,5 +3,6 @@
 if is_admin; then
     brew install ack git bash-completion
 else
-    echo must be run as admin
+    echo Must be run as admin. Try:
+	echo "su admin -c \"$0\""
 fi
