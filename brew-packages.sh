baee314abaff19c6de5b64e0764c5d9cf3f84cfe
Jeremy Vinding <jeremy.vinding@closely.com>
2013-11-04 15:50:47 -0700

    install git-flow

--- brew-packages.sh
+++ brew-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . ~/Environment/admin
 if is_admin; then
-    brew install ack git bash-completion
+    brew install ack git bash-completion git-flow
 else
     echo Must be run as admin. Try:
 	echo "su admin -c \"$0\""
