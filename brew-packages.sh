2d82d96fe7a2323b5e391144808eabff5db4ae1a
Jeremy Vinding <jeremy.vinding@closely.com>
2014-02-03 13:57:07 -0700

    install quicklook packages

--- brew-packages.sh
+++ brew-packages.sh
@@ -2,6 +2,13 @@
 . ~/Environment/admin
 if is_admin; then
     brew install ack git bash-completion git-flow
+
+    # cask
+    brew tap phinze/cask
+    brew install brew-cask
+
+    # quicklook plugins
+    brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package
 else
     echo Must be run as admin. Try:
 	echo "su admin -c \"$0\""
