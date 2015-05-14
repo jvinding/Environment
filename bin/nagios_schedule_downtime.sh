617e917fd99c61b2e59c9289497ed2e178a5c24b
Jeremy Vinding <jeremy.vinding@closely.com>
2015-05-13 20:31:02 -0600

    make nagios_schedule_downtime automatically schedule 90 minutes of downtime, begining immediately, for the passed in hosts rather than having to edit the script with the times

--- bin/nagios_schedule_downtime.sh
+++ bin/nagios_schedule_downtime.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -eu
 
 ## HOSTS=(api-production3 api-production4 api-qa1 api-staging1 api-staging2 bucket1 crawl-prod1 crawl-qa1 crawl-staging1 db-production db-production1 db-qa1 db-staging1 db-staging2 logstash metadata-db metadata-production metadata-qa metadata-staging redis-prod1)
-HOSTS=(kamino)
+HOSTS=("$@")
 
 if [ -z "${NAGIOS_USERNAME:-}" ] || [ -z "${NAGIOS_PASSWORD:-}" ]; then
     echo "NAGIOS_USERNAME and NAGIOS_PASSWORD must be set"
     exit 1
+fi
+
+if [ 0 == $# ]; then
+    echo
+    echo "At least 1 host must be passed on the command line" >&2
+    echo
+    echo "Schedules 30 minutes of down time for the hosts passed in on the command line"
+    echo "Usage: $0 <hostname 1>[..<hostname N>]"
 fi
 
 NAGIOS_HOST=admin.closely.com
@@ -18,10 +26,11 @@ NAGIOS_PATH=/nagios/cgi-bin
 ## now + 20 minutes on linux
 ## date -d "@$(($(date +%s) + 1200))" '+%m-%d-%Y+%H:%M:%S'
 ## MM-DD-YY+HH:mm:ss
-DOWNTIME_START="03-07-2015+03:00:00"
-DOWNTIME_END="03-07-2015+19:00:00"
+DOWNTIME_START=$(date -u +%m-%d-%Y+%H:%M:%S)
+DOWNTIME_END=$(date -u -v +90M +%m-%d-%Y+%H:%M:%S)
 
 for host in ${HOSTS[@]}; do
     echo -n "${host}: "
-    curl -s --basic -u "${NAGIOS_USERNAME}:${NAGIOS_PASSWORD}" "https://${NAGIOS_HOST}/${NAGIOS_PATH}/cmd.cgi?cmd_typ=55&cmd_mod=2&host=${host}&com_author=jvinding&com_data=rackspace+outage&trigger=0&start_time=${DOWNTIME_START}&end_time=${DOWNTIME_END}&fixed=1&childoptions=1&btnSubmit=Commit" | perl -n -e '/(?:error|info)Message.>*([^<]+)/ && print "$1\n"'
+    echo curl -s --basic -u "${NAGIOS_USERNAME}:${NAGIOS_PASSWORD}" "https://${NAGIOS_HOST}/${NAGIOS_PATH}/cmd.cgi?cmd_typ=55&cmd_mod=2&host=${host}&com_author=jvinding&com_data=rackspace+outage&trigger=0&start_time=${DOWNTIME_START}&end_time=${DOWNTIME_END}&fixed=1&childoptions=1&btnSubmit=Commit"
+    curl -s --basic -u "${NAGIOS_USERNAME}:${NAGIOS_PASSWORD}" "https://${NAGIOS_HOST}/${NAGIOS_PATH}/cmd.cgi?cmd_typ=55&cmd_mod=2&host=${host}&com_author=jvinding&com_data=outage&trigger=0&start_time=${DOWNTIME_START}&end_time=${DOWNTIME_END}&fixed=1&childoptions=1&btnSubmit=Commit" | perl -n -e '/(?:error|info)Message.>*([^<]+)/ && print "$1\n"'
 done
