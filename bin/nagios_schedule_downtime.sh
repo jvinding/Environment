7a00193312432a01b96404135ba663c8c27fad5f
Jeremy Vinding <jeremy.vinding@closely.com>
2015-05-27 15:43:17 -0600

    add '-l' to enable one to specify the length of downtime in the nagios downtime script

--- bin/nagios_schedule_downtime.sh
+++ bin/nagios_schedule_downtime.sh
@@ -1,20 +1,39 @@
 #!/bin/bash -eu
 
-## HOSTS=(api-production3 api-production4 api-qa1 api-staging1 api-staging2 bucket1 crawl-prod1 crawl-qa1 crawl-staging1 db-production db-production1 db-qa1 db-staging1 db-staging2 logstash metadata-db metadata-production metadata-qa metadata-staging redis-prod1)
-HOSTS=("$@")
-
 if [ -z "${NAGIOS_USERNAME:-}" ] || [ -z "${NAGIOS_PASSWORD:-}" ]; then
     echo "NAGIOS_USERNAME and NAGIOS_PASSWORD must be set"
     exit 1
 fi
 
-if [ 0 == $# ]; then
+usage() {
     echo
-    echo "At least 1 host must be passed on the command line" >&2
+    echo "Schedules of downtime for the hosts passed in on the command line"
+    echo "Usage: $0 [-l <time>] <hostname 1>[..<hostname N>]"
+    echo "    -l: length of downtime, e.g. '90M' or '12H' (default: 90M)"
     echo
-    echo "Schedules 30 minutes of down time for the hosts passed in on the command line"
-    echo "Usage: $0 <hostname 1>[..<hostname N>]"
+}
+
+LENGTH=90M
+
+if [ 0 != $# ] && [ "-l" == "$1" ]; then
+    if [ $# -ge 2 ] && [[ "$2" =~ ^[0-9]+[MHD]$ ]]; then
+        LENGTH=$2
+        shift
+        shift
+    else
+        echo "Invalid length paramteter" >&2
+        usage
+        exit 1
+    fi
 fi
+
+if [ 0 == $# ]; then
+    echo "At least 1 host must be passed on the command line" >&2
+    usage
+    exit 1
+fi
+
+HOSTS=("$@")
 
 NAGIOS_HOST=admin.closely.com
 NAGIOS_PATH=/nagios/cgi-bin
@@ -27,7 +46,7 @@ NAGIOS_PATH=/nagios/cgi-bin
 ## date -d "@$(($(date +%s) + 1200))" '+%m-%d-%Y+%H:%M:%S'
 ## MM-DD-YY+HH:mm:ss
 DOWNTIME_START=$(date -u +%m-%d-%Y+%H:%M:%S)
-DOWNTIME_END=$(date -u -v +90M +%m-%d-%Y+%H:%M:%S)
+DOWNTIME_END=$(date -u -v +${LENGTH} +%m-%d-%Y+%H:%M:%S)
 
 for host in ${HOSTS[@]}; do
     echo -n "${host}: "
