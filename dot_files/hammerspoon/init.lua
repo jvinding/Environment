a7d2eeb881d6de01ae29ae7aeefbb2cf286c5467
Jeremy Vinding <jeremy.vinding@closely.com>
2017-07-26 11:06:07 -0600

    make the app section slightly easier to look at

--- dot_files/hammerspoon/init.lua
+++ dot_files/hammerspoon/init.lua
@@ -6,32 +6,33 @@ local hyper = {"cmd", "alt", "ctrl", "shift"}
 --
 -- Open Applications
 --
-local appEmailClient = "Kiwi for Gmail"
--- local appEmailClient = "Postbox"
--- local appEmailClient = "Airmail"
-local appIdeIos = "AppCode"
--- local appIdeIos = "AppCode-EAP"
-local appIdeJava = "IntelliJ IDEA"
--- local appIdeJava = "IntelliJ IDEA-EAP"
-local appIdeApple = 'Xcode'
--- local appIdeApple = 'Xcode-beta'
+local apps = {
+  { "1", "1Password 6" },
+  { "A", "Android Studio" },
+  { "B", "Brave" },
+  { "C", "AppCode" },
+  -- { "C", "AppCode-EAP" },
+  { "E", "Messages" },
+  { "G", "Google Chrome" },
+  { "I", "IntelliJ IDEA" },
+  -- { "I", "IntelliJ IDEA-EAP" },
+  { "J",  "Kiwi for Gmail" },
+  -- { "J",  "Postbox" },
+  -- { "J",  "Airmail" },
+  { "K", "Skype" },
+  { "L", "Slack" },
+  { "N", "Navicat Essentials for PostgreSQL" },
+  { "R", "iTerm" },
+  { "S", "Tower" },
+  { "T", "MacVim" },
+  { "X", "Xcode" },
+  -- { "X", "Xcode-beta" },
+  { "Z", "Zeplin" }
+}
+for i, app in ipairs(apps) do
+  hotkey.bind(hyper, app[1], function() application.launchOrFocus(app[2]) end)
+end
 
-hotkey.bind(hyper, '1', function() application.launchOrFocus("1Password 6") end)
-hotkey.bind(hyper, 'A', function() application.launchOrFocus("Android Studio") end)
-hotkey.bind(hyper, 'B', function() application.launchOrFocus("Brave") end)
-hotkey.bind(hyper, 'C', function() application.launchOrFocus(appIdeIos) end)
-hotkey.bind(hyper, 'E', function() application.launchOrFocus("Messages") end)
-hotkey.bind(hyper, 'G', function() application.launchOrFocus("Google Chrome") end)
-hotkey.bind(hyper, 'I', function() application.launchOrFocus(appIdeJava) end)
-hotkey.bind(hyper, 'J', function() application.launchOrFocus(appEmailClient) end)
-hotkey.bind(hyper, 'K', function() application.launchOrFocus("Skype") end)
-hotkey.bind(hyper, 'L', function() application.launchOrFocus("Slack") end)
-hotkey.bind(hyper, 'N', function() application.launchOrFocus("Navicat Essentials for PostgreSQL") end)
-hotkey.bind(hyper, 'R', function() application.launchOrFocus("iTerm") end)
-hotkey.bind(hyper, 'S', function() application.launchOrFocus("Tower") end)
-hotkey.bind(hyper, 'T', function() application.launchOrFocus("MacVim") end)
-hotkey.bind(hyper, 'X', function() application.launchOrFocus(appIdeApple) end)
-hotkey.bind(hyper, 'Z', function() application.launchOrFocus("Zeplin") end)
 --
 -- /Open Applications
 --
