22227746e6f5e710225ba28c6c46cccbdbbf3ad2
Jeremy Vinding <jvinding@thrivehive.com>
2019-04-03 10:23:46 -0600

    Disable key bindings when using screen sharing

--- dot_files/hammerspoon/app_bindings.lua
+++ dot_files/hammerspoon/app_bindings.lua
@@ -28,5 +28,10 @@ local apps = {
 }
 
 for key, app in pairs(apps) do
-  hs.hotkey.bind(hyper, key, function() hs.application.launchOrFocus(app) end)
+    local binding = hs.hotkey.bind(hyper, key, function() hs.application.launchOrFocus(app) end)
+    local wf = hs.window.filter
+    screenSharingFilter = wf.new("Screen Sharing")
+    screenSharingFilter:subscribe(wf.windowFocused, function() binding:disable() end)
+    screenSharingFilter:subscribe(wf.windowUnfocused, function() binding:enable() end)
 end
+
