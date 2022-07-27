8019002614e8840ec1708499f4e3cb60e5ab841a
Jeremy Vinding <jeremy.vinding@vizio.com>
2022-07-27 10:20:15 -0600

    feat(hammerspoon): replace my code with a couple of spoons

--- dot_files/hammerspoon/init.lua
+++ dot_files/hammerspoon/init.lua
@@ -1,7 +1,15 @@
 hyper = {"cmd", "alt", "ctrl", "shift"}
 
-require 'config_watcher'
 require 'app_bindings'
-require 'mouse_locator'
 require 'window_resize'
 require 'caffeinate'
+
+hs.loadSpoon("MouseCircle")
+spoon.MouseCircle:bindHotkeys({["show"]={hyper, "f1"}})
+
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+hs.keycodes.inputSourceChanged(function() hs.reload() end)
+
+hs.alert.show("Config Loaded")
