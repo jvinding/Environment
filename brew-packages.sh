6944bfb0dcad472152eb222f6660b30be89dc9de
Jeremy Vinding <>
2024-04-06 09:32:54 -0600

    feat: update the brew packages script

--- brew-packages.sh
+++ brew-packages.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-. ~/Environment/admin
-if is_admin; then
-    brew install ack git bash-completion git-flow
 
-    # tmux
-    brew install tmux reattach-to-user-namespace
+brew tap homebrew/cask-fonts
 
-    # cask
-    brew tap phinze/cask
-    brew install brew-cask
-
-    # shell extensions
-    brew install z
-
-    # quicklook plugins
-    brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package
-else
-    echo Must be run as admin. Try:
-    echo "su admin -c \"$0\""
-fi
+brew install bat eza fzf gh git-delta neovide
+brew install --cask font-sauce-code-pro-nerd-font karabiner-elements warp hammerspoon
